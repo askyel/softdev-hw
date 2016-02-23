c2e3b55c64f8343a0bd96de61554ebdb93ea6b32
Ariel Levy <ariel.s.levy1@gmail.com>
2016-02-22 19:45:28 -0500

    dot works

--- hw02/animate.js
+++ hw02/animate.js
@@ -1,8 +1,25 @@
 var pg = document.getElementById("playground");
 var ctx = pg.getContext("2d");
 
-var drawDot = function() {
-    ctx.clearRect( 0, 0, pg.width, pg.height );
-    if (growing) { radius += 1; }
+var radius = 1;
+var growing = true;
+
+var drawDot = function drawDot() {
+   	if (radius >= Math.max(pg.width/2, pg.height/2)) { growing = false; }
+	else if (radius <= 0) { growing = true; }
+
+	if (growing) { radius += 1; }
     else { radius -= 1; }
+
+	console.log("drawing");
+    ctx.clearRect( 0, 0, pg.width, pg.height );
+	ctx.beginPath();
+	ctx.fillStyle = "#2CF";
+	ctx.arc( pg.width/2, pg.height/2, radius, 0, 2*Math.PI );
+	ctx.fill();
+
+	window.requestAnimationFrame(drawDot);
 }
+
+var button = document.getElementById("draw");
+button.addEventListener( "click", drawDot );
