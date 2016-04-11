8896df5139a565c35d62e505c37ea73db80ca8df
Ariel Levy <ariel.s.levy1@gmail.com>
2016-04-11 19:20:44 -0400

    6 done

--- hw06/list_comp.py
+++ hw06/list_comp.py
@@ -13,3 +13,23 @@ def valid( pwd ):
 
 print valid( "a123ber" )  # False
 print valid( "4bG" )   	  # True
+
+SP = ".?!#&,;:-_*"
+
+def valid2( pwd ):
+	score = 1
+	if len(pwd) >= 6:
+		score += 2
+	if [ x for x in pwd if x in UC ] != []:
+		score += 2
+	if [ x for x in pwd if x in LC ] != []:
+		score += 2
+	if [ x for x in pwd if x in NUM ] != []:
+		score += 2
+	if len(pwd) >= 8:
+		score += 1
+	return score
+
+print valid2( "asld" )  # 3
+print valid2( "aGsd,34" )  # 9 
+print valid2( "asdlfkj3049u509u$ou3w094iw:SFD" )   # 10
